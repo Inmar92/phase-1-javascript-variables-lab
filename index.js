41b6bf7bd048e1a800e392efd17ae181510682ff
inmar salinas <inmarsalinas@yahoo.com>
2023-03-29 12:25:38 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const companyName = ('Scuber');
+function CompanyName() {
+    let companyName = 'Scuber'
+}
+
+const mostProfitableNeighborhood = ('Chelsea');
+function MostProfitableNeighborhood() {
+    let mostProfitableNeighborhood = 'Chelsea'
+}
+
+const companyCeo = ('Susan Smith');
+function CompanyCeo() {
+    let companyCeo = 'Susan Smith'
+}
